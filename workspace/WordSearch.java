91650d7b23e1f50ed42ccf1f4a80454e9f952f86
Naresh Nelavalli <naresh.nelavalli@gmail.com>
2018-01-28 12:50:20 -0600

    Update WordSearch.java
    Updated, identified few failed test scenarios

--- workspace/WordSearch.java
+++ workspace/WordSearch.java
@@ -5,10 +5,12 @@ import java.util.Set;
 public class WordSearch 
 {
 	Set<String> output = new HashSet<String>();
+	TrieImpl trie = new TrieImpl();
+	char[][] board ;
 	public Set<String> findWords(char[][] board, String[] words) 
 	{
-		TrieImpl ti = new TrieImpl();
-		int [][] isVisited = new int[board.length][board.length] ;
+		int maxLength = board[0].length;
+		this.board = board;
 
 		for(String word : words)
 		{
@@ -18,7 +20,8 @@ public class WordSearch
 		{
 			for(int j=0;j<board.length;j++)
 			{
-				checkWords(ti, "",i, j,board ,isVisited);
+				int [][] isVisited = new int[board.length][maxLength] ;
+				checkWords("",i, j,isVisited);
 			}
 		}
 
@@ -26,11 +29,10 @@ public class WordSearch
 
 	}
 
-	public void checkWords(TrieImpl trie, String soFar, int x, int y, char [][] board,int [][] isVisited )
+	public void checkWords(String soFar, int x, int y,int [][] isVisited )
 	{
-		if(x >= board.length || y >= board.length ||  x < 0 || y < 0)
+				if(x >= board.length || y >= board[x].length ||  x < 0 || y < 0)
 		{
-			// out of bound // index xception
 			return;
 		}
 		if(isVisited[x][y] == 1)
@@ -38,12 +40,16 @@ public class WordSearch
 		soFar = soFar + board[x][y];
 		isVisited[x][y] =1;
 		List<String> wordsWithPrefix = trie.findWordsWithPrefix(soFar);
+
 		if(wordsWithPrefix.size() > 0)
 		{
 			for(String word:wordsWithPrefix)
 			{
-				if(trie.isThataWord(word) && soFar.equals(word))
-					output.add(word);
+				if(word.length() == soFar.length())
+				{
+					if(trie.isThataWord(word) && soFar.equals(word))
+						output.add(word);
+				}
 			}
 		}
 		else
@@ -52,27 +58,38 @@ public class WordSearch
 			isVisited[x][y] =0;
 			return;
 		}
-		if( x < board.length -1  )
+		if( x < board.length - 1 )
 		{
-			checkWords( trie,  soFar,  x +1,  y , board ,isVisited); 
-			isVisited[x+1][y] =0;
+			if(isVisited[x+1][y]!=1)
+			{
+				checkWords(soFar,  x +1,  y  ,isVisited); 
+				isVisited[x+1][y] =0;
+			}
+		}
+		if( y < board[x].length -1 )
+		{
+			if(isVisited[x][y+1]!=1)
+			{
+				checkWords( soFar,  x ,  y + 1,isVisited );
+				isVisited[x][y+1] =0;
+			}
 		}
 		if( x>0 )
 		{
-			checkWords( trie,  soFar,  x - 1,  y, board ,isVisited);
-			isVisited[x-1][y] =0;
-		}
-		if( y < board.length -1 )
-		{
-			checkWords( trie,  soFar,  x ,  y + 1, board,isVisited );
-			isVisited[x][y+1] =0;
+			if(isVisited[x-1][y]!=1)
+			{
+				checkWords(soFar,  x - 1,  y ,isVisited);
+				isVisited[x-1][y] =0;
+			}
 		}
 		if( y>0 )
 		{
-			checkWords( trie,  soFar,  x ,  y-1, board ,isVisited);     
-			isVisited[x][y-1] =0;
+			if(isVisited[x][y-1]!=1)
+			{
+				checkWords(soFar,  x ,  y-1 ,isVisited);     
+				isVisited[x][y-1] =0;
+			}
 
 		}
-
 	}
 }
